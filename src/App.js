40f1a6f41b2e9e9d9e2b5d888e783115ff8ec575
hubdncljh97 <hubdncljh97@gmail.com>
2022-08-02 22:00:15 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,67 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+import { Container, Nav, Navbar, Row, Col } from 'react-bootstrap';
+import data from './data';
 
 function App() {
+  let [shoes] = useState(data);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar bg="dark" variant="dark">
+        <Container>
+          <Navbar.Brand href="#home">ENFP쇼핑몰</Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#features">Features</Nav.Link>
+            <Nav.Link href="#pricing">Pricing</Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
+      <div className="main-bg"></div>
+      <Container className="sell-list">
+        <Row>
+          {shoes.map((item, index) => {
+            return (
+              <>
+                <SellItem
+                  index={index}
+                  title={item.title}
+                  content={item.content}
+                  price={item.price}
+                />
+              </>
+            );
+          })}
+        </Row>
+      </Container>
     </div>
   );
 }
 
 export default App;
+
+// interface SellItemProps { > TypeScript에서 사용하는 문법임
+//   index: number
+// title: String
+// content: string
+// price: string
+// }
+function SellItem(props) {
+  return (
+    <>
+      <Col key={props.index} sm>
+        <img
+          src={`https://codingapple1.github.io/shop/shoes${
+            props.index + 1
+          }.jpg`}
+          alt="상품사진"
+          width="100%"
+        />
+        <h4>{props.title}</h4>
+        <p>{props.content}</p>
+        <p>{props.price}</p>
+      </Col>
+    </>
+  );
+}
