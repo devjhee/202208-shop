265055e41a599c10268428d604ea52e4508801de
devjhee <devjhee@gmail.com>
2022-08-03 23:37:53 +0900

    220803_useEffect(1)

--- src/routes/detail.js
+++ src/routes/detail.js
@@ -1,16 +1,46 @@
 // import { useNavigate } from 'react';
+import { useEffect } from 'react';
 import { useParams } from 'react-router-dom';
+import { useState } from 'react';
 
 function Detail(props) {
   const { paraid } = useParams();
-  console.log('check this : ' + paraid);
+  //console.log('check this : ' + paraid);
   const myProduct = props.data.find(function (i) {
     return i.id == paraid;
   });
 
+  useEffect(() => {
+    //side-effect : 부가기능 > useEffect : 부가기능함수
+    //mount (+ update)될 때 실행되는 소스
+    //어려운 연산이나 서버에서 데이터를 가져오는 작업, 타이머 장착 등 시간이 소요되는 소스를 주로 작성함
+    //html 렌더링 후에 동작함
+    //<React.StrictMode> 설정되어있어 console이 2번 찍힘
+
+    console.log('bye');
+    setTimeout(() => {
+      setDisplay(!display);
+    }, 2000);
+
+    return () => {
+      console.log(
+        'useEffect 동작 전에 실행됨 / 기존 소스와 충돌되어 제거하고 싶을 때 사용'
+      );
+    };
+  }, []); //[]는 useEffect 실행조건 기재하는 자리
+
+  const [count, setCount] = useState(0);
+  const [display, setDisplay] = useState(false);
+
   return (
     <>
       <div className="container">
+        <br />
+        {!display && (
+          <>
+            <div className="alert alert-warning">2초 이내 구매시 할인</div>
+          </>
+        )}
         <div className="row">
           <div className="col-md-6">
             <img
@@ -25,6 +55,15 @@ function Detail(props) {
             <p>{myProduct.content}</p>
             <p>{myProduct.price}</p>
             <button className="btn btn-danger">주문하기</button>
+            <br />
+            <br />
+            <button
+              className="btn"
+              style={{ backgroundColor: 'blue', color: 'white' }}
+              onClick={() => setCount(count + 1)}
+            >
+              Like {count}
+            </button>
           </div>
         </div>
       </div>
